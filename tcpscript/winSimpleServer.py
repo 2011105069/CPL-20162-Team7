a4b2b1767b82d4b88d44c8d704ad457a354f3e77
Seongwook Kim <suelcun92@gmail.com>
2016-11-14 12:57:55 +0900

    Create winSimpleServer.py

---
+++ tcpscript/winSimpleServer.py
@@ -0,0 +1,34 @@
+import sys
+import socket
+import os
+
+
+#create a tcp/ip socket.
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the socket to the port
+s_addr = ('localhost', 306)
+print('start up on %s port %s' %s_addr)
+sock.bind(s_addr)
+sock.listen(1)
+
+
+while True:
+	print('wating for connection')
+	connection, c_addr = sock.accept()
+	#make img file name.
+
+	try:
+		print('connection from %s', c_addr)
+
+		while True:
+			data = connection.recv(20)
+
+			if data:
+				print(data)
+			else:
+				print('receive done ')
+				break
+				
+	finally:
+		pass
