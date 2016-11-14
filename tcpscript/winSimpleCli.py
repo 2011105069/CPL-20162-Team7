2e9079658b55d194f518f6fc6bd45882bf87a554
Seongwook Kim <suelcun92@gmail.com>
2016-11-14 13:02:22 +0900

    Update and rename totext.py to winSimpleCli.py

--- tcpscript/winSimpleCli.py
+++ tcpscript/winSimpleCli.py
@@ -1,19 +1,15 @@
 import sys
 import socket
-import time
-import errno
-import os
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s_addr = ('192.168.200.128', 306)
+s_addr = ('localhost', 306)
 sock.connect(s_addr)
 
 #sock.setblocking(0)
 
-#send image to server.
-
-
-img ='hi im seven\n'
-sock.send(img)
+#send tex to server.
+tex ='hi im seven\n'
+sock.send(str.encode(tex))
+print('sending done')
 
 sock.close()
